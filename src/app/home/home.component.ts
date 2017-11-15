77e34173b25e6c8902a376c3a48b431aaaa71832
dinkohodzic <dinkoakaneo@gmail.com>
2017-11-15 11:40:16 +0100

    first try at adding angular animations to the home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,31 @@ import {MatCardModule} from '@angular/material/card';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   encapsulation: ViewEncapsulation.None,
+  animations:[
+    trigger('anim1',[
+      transition('*=>*', [
+        query('.logo', style({opacity:0, transform: 'translateY(-100px)'})),
+        query('.logo',stagger('400ms',[
+          animate('500ms ease-in', style({opacity: 1, transform: 'translateY(0)'})),
+        ])),
+        query('.logo', [
+          animate(0, style('*'))
+        ])
+      ])
+    ]),
+    trigger('anim2',[
+      transition('*=>*', [
+        query('.cont', style({opacity:0, transform: 'translateX(-25%)'})),
+        query('.cont',stagger('400ms',[
+          animate('400ms ease-in', style({opacity: 1, transform: 'translateX(0)'})),
+        ])),
+        query('.cont', [
+          animate(0, style('*'))
+        ])
+      ])
+    ])
+    
+  ]
   
 })
 export class HomeComponent implements OnInit {
